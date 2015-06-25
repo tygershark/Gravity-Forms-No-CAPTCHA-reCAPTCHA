abc1a28b46d20821555f37d57f746049fdaa1de5
Spenser Hale <spenserhale@outlook.com>
2015-06-25 12:16:45 -0700

    changing label's for to match the select field's ID

--- gravity-forms-no-captcha-recaptcha/trunk/public/partials/gf-no-captcha-recaptcha-public-theme-dropdown.php
+++ gravity-forms-no-captcha-recaptcha/trunk/public/partials/gf-no-captcha-recaptcha-public-theme-dropdown.php
@@ -1,6 +1,6 @@
 <li class="recaptcha_theme_setting field_setting">
 
-	<label for="field_theme_setting"><?php _e( 'CAPTCHA Theme', 'gravityforms' ); ?></label>
+	<label for="field_recaptcha_theme_value"><?php _e( 'CAPTCHA Theme', 'gravityforms' ); ?></label>
 
 	<select id="field_recaptcha_theme_value" onchange="SetFieldProperty( 'recaptcha_theme', this.value );">
 		<option value="light">Light</option>
